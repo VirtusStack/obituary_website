b8554233103f66437939e2908e1490352fe05ed2
VirtusStack <payalgupta6403@gmail.com>
2025-08-19 11:27:31 +0530

    Update download_template.php

--- download_template.php
+++ download_template.php
@@ -1,9 +1,12 @@
 <?php
 session_start();
 require_once __DIR__ . "/config.php";
+require __DIR__ . "/vendor/autoload.php";
 
-// check login
+use Dompdf\Dompdf;
+use Dompdf\Options;
 
+// Check login
 if (!isset($_SESSION['user_id'])) {
     header("Location: auth/login.php");
     exit;
@@ -12,26 +15,47 @@ if (!isset($_SESSION['user_id'])) {
 $template = $_GET['template'] ?? '';
 $id = isset($_GET['obituary_id']) ? (int)$_GET['obituary_id'] : 0;
 
-
 if (!$template || !$id) {
     die("Invalid request");
 }
 
-// Validate template exists
 $valid_templates = ['template1', 'template2'];
 if (!in_array($template, $valid_templates)) {
-die("Invalid template");
+    die("Invalid template");
 }
 
-// Capyure the output of the template page as HTML string
+// Fetch obituary details
+$stmt = $conn->prepare("SELECT * FROM obituaries WHERE id = ?");
+$stmt->execute([$id]);
+$obituary = $stmt->fetch(PDO::FETCH_ASSOC);
+if (!$obituary) die("Obituary not found");
+
+// Capture template output
 ob_start();
-include __DIR__. "/templates/$template/$template.php";
+include __DIR__ . "/templates/$template/$template.php";
 $html = ob_get_clean();
 
-// Set headers to forse download as .html file
-header('Content-Type: text/html');
-header('Content-Disposition: attachment; filename="obituary_'.$id.'_'.$template.'.html"');
-echo $html;
+// Fix template CSS and image paths
+$template_base_url = "http://localhost/obituary_website/templates/$template/";
+$html = str_replace('href="', 'href="' . $template_base_url, $html);
+$html = str_replace('src="/obituary_website/uploads/', 'src="http://localhost/obituary_website/uploads/', $html);
+
+// Setup Dompdf
+$options = new Options();
+$options->set('isRemoteEnabled', true); // allow external CSS/images
+$options->set('defaultFont', 'Helvetica');
+$dompdf = new Dompdf($options);
+
+// Load HTML
+$dompdf->loadHtml($html);
+
+// Set paper size
+$dompdf->setPaper('A4', 'portrait');
+
+// Render PDF
+$dompdf->render();
+
+// Output PDF
+$filename = "obituary_{$id}_{$template}.pdf";
+$dompdf->stream($filename, ["Attachment" => false]);
 exit;
-
-
